e25c1e6e00218ece965c645460f40068de019588
Gab16El <noytte29@gmail.com>
2026-02-19 10:04:36 -0500

    feat: add file system, screens, packages and colors.ts

---
+++ src/screens/StoresScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StoresScreen = () => {
+    return (
+        <View>
+            <Text>StoresScreen</Text>
+        </View>
+    )
+}
+
+export default StoresScreen
---
+++ src/theme/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#02A9AB',
+    white: '#FFFFFF',
+    black: '#1A1A1A',
+    gray: '#6B7280',
+    grayLight: '#F3F4F6',
+    background: '#F9FAFB',
+}
