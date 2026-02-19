3abf13c8e66f959d39747b8a08e2ca4c69966757
Gab16El <noytte29@gmail.com>
2026-02-19 11:10:28 -0500

    chore: add mock stores object and add new imgs

---
+++ src/mocks/stores.mock.ts
@@ -0,0 +1,24 @@
+export const stores = [
+    {
+        "id": 1,
+        "name": "Chifa el Sol",
+        "bannerUrl": "../assets/stroes/chifa-el-sol.png",
+        "latitude": 40.7128,
+        "longitude": -74.0060,
+        "openTime": "09:00 AM",
+        "closeTime": "07:00 PM",
+        "distanceInKm": 1.5,
+        "isOpen": true
+    },
+    {
+        "id": 2,
+        "name": "Primos - Chiken Lovers Bar",
+        "bannerUrl": "../assets/stroes/primos.png",
+        "latitude": 34.0522,
+        "longitude": -118.2437,
+        "openTime": "10:00 AM",
+        "closeTime": "08:30 PM",
+        "distanceInKm": 3.2,
+        "isOpen": true
+    }
+]
