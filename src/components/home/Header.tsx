00675c6c5297966e8d884b9a7f81bc2ef06090e9
Gab16El <noytte29@gmail.com>
2026-02-19 11:19:49 -0500

    feat: add header component to the homescreen and replace the svg for png

---
+++ src/components/home/Header.tsx
@@ -0,0 +1,49 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { colors } from '../../theme/colors'
+
+export const Header = () => {
+    return (
+        <View>
+            <View style={styles.headerContainer}>
+                <Image source={require('../../assets/logo/cirkula.png')} style={styles.headerImg} />
+                <View style={styles.headerOverlay} />
+            </View>
+            <View style={styles.headerLabelsContainer}>
+                <Text style={styles.title}>Bienvenido</Text>
+                <Text style={styles.subTitle}>Encuentra lo que más te gusta</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        height: 300,
+        backgroundColor: colors.black,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerOverlay: {
+        ...StyleSheet.absoluteFill,
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    },
+    headerImg: {
+        width: '100%',  
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    headerLabelsContainer: {
+        position: 'absolute',
+        top: 220,
+        left: 20,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: colors.white,
+    },
+    subTitle: {
+        fontSize: 16,
+        color: colors.white,
+    },
+})
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Dimensions, ScrollView, StyleSheet } from 'react-native'
+import { colors } from '../theme/colors';
+import { Header } from '../components/home/Header';
+
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <ScrollView style={[styles.wrap, { width }]}>
+        <Header />
+      </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  wrap: {
+    width: width,
+    height: height * 0.25
+  },
+})
