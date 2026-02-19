72608ec8916ab2f55ae89b53479de5df42bdd400
Gab16El <noytte29@gmail.com>
2026-02-19 10:54:09 -0500

    chore: put the provider in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { AppNavigator } from './src/navigation/AppNavigator'
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
 
 export const App = () => {
   return (
-    <AppNavigator />
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   )
 }
