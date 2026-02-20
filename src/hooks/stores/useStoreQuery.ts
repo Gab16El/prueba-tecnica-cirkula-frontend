7f63163c5519a6f263a94e3f886c8c1c987854fc
Gab16El <noytte29@gmail.com>
2026-02-19 19:04:49 -0500

    feat: add useStoreQuery

---
+++ src/hooks/stores/useStoreQuery.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { getStores } from '../../services/store.service';
+
+// Lima, Miraflores como coordenadas por defecto
+// ESTO ES DE LA WEB, PRACTICAMENTE ES LO MISMO
+// LA DIFERENCIA ES QUE ACA SI ME PIDE QUE LE ENVIE MIS COORDENADAS REALES
+const DEFAULT_COORDS = {
+    latitude: -12.1211,
+    longitude: -77.0282,
+};
+
+interface Coords {
+    latitude: number;
+    longitude: number
+}
+
+export const useStoresQuery = (coords: Coords) => {
+    return useQuery({
+        queryKey: ['stores', coords.latitude, coords.longitude],
+        queryFn: () => getStores(coords),
+        select: (data) => data.stores,
+    });
+};
